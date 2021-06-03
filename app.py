0c59949db4104cc582732a849bb49a2f0f389b06
Sean Yu <sjy2129@columbia.edu>
2021-06-03 16:47:22 +0800

    First commit

---
+++ app.py
@@ -0,0 +1,42 @@
+from flask import Flask,render_template,url_for, request
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+from sudoku_solver import solver
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///test.db'
+db = SQLAlchemy(app)
+
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(200),nullable=False)
+    date_created=db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __repr__(self):
+        return '<Task %r>' %self.id
+
+@app.route('/',methods=['POST','GET'])
+def index():
+    if request.method =='POST':
+        s=''
+        for i in ["box_1","box_2","box_3","box_4","box_5","box_6","box_7","box_8","box_9","box_10","box_11","box_12","box_13","box_14","box_15","box_16","box_17","box_18","box_19","box_20","box_21","box_22","box_23","box_24","box_25","box_26","box_27","box_28","box_29","box_30","box_31","box_32","box_33","box_34","box_35","box_36","box_37","box_38","box_39","box_40","box_41","box_42","box_43","box_44","box_45","box_46","box_47","box_48","box_49","box_50","box_51","box_52","box_53","box_54","box_55","box_56","box_57","box_58","box_59","box_60","box_61","box_62","box_63","box_64","box_65","box_66","box_67","box_68","box_69","box_70","box_71","box_72","box_73","box_74","box_75","box_76","box_77","box_78","box_79","box_80","box_81"]:
+            sudoku_square=request.form[i]
+            if len(sudoku_square)==0:
+                s+='0'
+            else:  
+                s+=sudoku_square
+        #a='003020600900305001001806400008102900700000008006708200002609500800203009005010300'
+        print(s)
+        print(len(s))
+        if len(s)>81:
+            return render_template('error.html')
+        sol=solver(s)
+        
+        if sol==False:
+            return render_template('error.html')
+        else:
+            return render_template('sol.html',sol=sol)
+    else:
+        return render_template('index.html')
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ sudoku_solver.py
@@ -0,0 +1,299 @@
+from copy import deepcopy
+import time
+import sys
+"""
+Each sudoku board is represented as a dictionary with string keys and
+int values.
+e.g. my_board['A1'] = 8
+"""
+
+ROW = "ABCDEFGHI"
+COL = "123456789"
+
+
+def print_board(board):
+    """Helper function to print board in a square."""
+    print("-----------------")
+    for i in ROW:
+        row = ''
+        for j in COL:
+            row += (str(board[i + j]) + " ")
+        print(row)
+
+
+def board_to_string(board):
+    """Helper function to convert board dictionary to string for writing."""
+    ordered_vals = []
+    for r in ROW:
+        for c in COL:
+            ordered_vals.append(str(board[r + c]))
+    return ''.join(ordered_vals)
+
+
+def check(board,val,row,col):
+    #check col
+    checkpos=row+str(col)
+    for i in ROW:
+        
+        pos=i+str(col)
+        if board[pos]==val and pos!=checkpos:
+            return False
+        
+    #check row
+    for k in COL:
+        pos=row+str(k)
+        if board[pos]==val and pos!=checkpos:
+            return False
+
+    #check box
+    if row in ROW[0:3]:
+        if str(col) in COL[0:3]:
+            for l in ROW[0:3]:
+                for m in COL[0:3]: 
+                    if board[l+m]==val and l+m!=checkpos:
+                        return False                    
+        if str(col) in COL[3:6]:
+            for l in ROW[0:3]:
+                    for m in COL[3:6]: 
+                        if board[l+m]==val and l+m!=checkpos:
+                            return False
+        if str(col) in COL[6:9]:
+            for l in ROW[0:3]:
+                    for m in COL[6:9]:
+                        if board[l+m]==val and l+m!=checkpos:
+                            return False
+    if row in ROW[3:6]:
+        if str(col) in COL[0:3]:
+            for l in ROW[3:6]:
+                for m in COL[0:3]:
+                    if board[l+m]==val and l+m!=checkpos:
+                        return False
+                    
+        if str(col) in COL[3:6]:
+            for l in ROW[3:6]:
+                    for m in COL[3:6]: 
+                        if board[l+m]==val and l+m!=checkpos:
+                            return False
+        if str(col) in COL[6:9]:
+            for l in ROW[3:6]:
+                    for m in COL[6:9]: 
+                        if board[l+m]==val and l+m!=checkpos:
+                            return False
+            
+    if row in ROW[6:9]:
+        if str(col) in COL[0:3]:
+            for l in ROW[6:9]:
+                for m in COL[0:3]:
+                    if board[l+m]==val and l+m!=checkpos:
+                        return False
+                    
+        if str(col) in COL[3:6]:
+            for l in ROW[6:9]:
+                    for m in COL[3:6]:
+                        if board[l+m]==val and l+m!=checkpos:
+                            return False
+        if str(col) in COL[6:9]:
+            for l in ROW[6:9]:
+                    for m in COL[6:9]:
+                        if board[l+m]==val and l+m!=checkpos:
+                            return False
+
+    return True
+
+def mrv(domain_dict, board):
+    unassigned_tile = [tile for tile in domain_dict.keys() if board[tile] == 0]
+    return min(unassigned_tile, key=lambda tile: len(domain_dict[tile]))
+
+def backtracking(board):
+    d=domains(board)
+    finished_board = bt({},board,d)
+    return finished_board
+    
+
+def bt(assignment, board,domains):
+    """Takes a board and returns solved board."""
+    cur_domain=deepcopy(domains)    
+    
+
+
+    x = [tile for tile in domains.keys() if board[tile] == 0]
+    if not x:
+        return board
+
+    pos=mrv(domains, board)
+    vals=cur_domain[pos]
+    
+    for k in vals:
+        if forwardcheck(board,k,pos)==True:
+            d_copy=deepcopy(domains)
+
+            board[pos]=k
+
+            assignment_copy = deepcopy(assignment)
+            assignment_copy[pos] = k
+            d=updatedomains(cur_domain,board)
+
+            result=bt(assignment_copy,board, d)
+            if result!=False:
+                return result
+            
+            board[pos]=0
+        d =deepcopy(domains)
+    return False
+
+def forwardcheck(board, pos, k):
+    board[pos]=k
+    allvars=[]
+    for i in ROW:
+        for j in COL:
+            if board[i+j]==0:
+                posvar=[]
+                for k in range(1,10):
+                    if check(board,k,i,j):
+                        posvar.append(k)
+                allvars.append(posvar)
+    if any (x==[] for x in allvars):
+        return False
+    else:
+        return True
+
+def checkdone(board):
+    for i in ROW:
+        for j in COL:
+            if check(board,board[i+j],i,j)==False:
+               return False 
+    return True
+
+def domains(board):
+    allvars=[]
+    allboardpos=[]
+    varsdict={}
+    for i in ROW:
+        for j in COL:
+            if board[i+j]==0:
+                posvar=[]
+                boardpos=str(i+j)
+                allboardpos.append(boardpos)
+                for k in range(1,10):
+                    if check(board,k,i,j):
+                        posvar.append(k)
+                allvars.append(posvar)
+                varsdict[str(i+j)]=posvar
+    if len(allvars)>0:
+        minvars=min(allvars)
+        ind=allvars.index(minvars)
+        orddict={a: b for a, b in sorted(varsdict.items(), key=lambda item: len(item[1]))}
+        return orddict
+        
+    return False
+
+def updatedomains(domain,board):
+    allvars=[]
+    allboardpos=[]
+    varsdict={}
+    for i in domain:
+        posvar=[]
+        boardpos=i
+        allboardpos.append(boardpos)
+        for k in range(1,10):
+            if check(board,k,i[0],i[1]):
+                posvar.append(k)
+        allvars.append(posvar)
+        varsdict[i]=posvar
+    if len(allvars)>0:
+        minvars=min(allvars)
+        ind=allvars.index(minvars)
+        orddict={a: b for a, b in sorted(varsdict.items(), key=lambda item: len(item[1]))}
+        return orddict
+        
+    return False
+def solver(line):
+    
+    board = { ROW[r] + COL[c]: int(line[9*r+c])
+                  for r in range(9) for c in range(9)}
+    print(board)
+    if initial_check(board):
+        sol=backtracking(board)
+        return(sol)
+    else:
+        return False
+#    return backtracking(board)
+def initial_check(board):
+    for i in ROW:
+        row_val=[]
+        for j in COL:
+            row_val.append(board[i+j])
+        no_zero_row=list(filter(lambda a: a != 0, row_val))
+        if len(no_zero_row)!=len(set(no_zero_row)):    
+            return False
+
+    for i in COL:
+        col_val=[]
+        for j in ROW:
+            col_val.append(board[j+i])
+        no_zero_col=list(filter(lambda a: a != 0, col_val))
+        if len(no_zero_col)!=len(set(no_zero_col)):    
+            return False
+
+    box1=[]
+    box2=[]
+    box3=[]
+    for i in ROW[0:3]:   
+        for j in COL[0:3]:
+            box1.append(board[i+j])
+        no_zero_box_1=list(filter(lambda a: a != 0, box1))
+        if len(no_zero_box_1)!=len(set(no_zero_box_1)):
+            return False
+        for j in COL[3:6]:   
+            box2.append(board[i+j])
+        no_zero_box_2=list(filter(lambda a: a != 0, box2))
+        if len(no_zero_box_2)!=len(set(no_zero_box_2)):
+            return False
+        for j in COL[6:9]:
+            box3.append(board[i+j])
+        no_zero_box_3=list(filter(lambda a: a != 0, box3))
+        if len(no_zero_box_3)!=len(set(no_zero_box_3)):
+            return False
+
+    box1=[]
+    box2=[]
+    box3=[]
+    for i in ROW[3:6]:   
+        for j in COL[0:3]:
+            box1.append(board[i+j])
+        print((box1))
+        no_zero_box_1=list(filter(lambda a: a != 0, box1))
+        if len(no_zero_box_1)!=len(set(no_zero_box_1)):
+            return False
+        for j in COL[3:6]:   
+            box2.append(board[i+j])
+        no_zero_box_2=list(filter(lambda a: a != 0, box2))
+        if len(no_zero_box_2)!=len(set(no_zero_box_2)):
+            return False
+        for j in COL[6:9]:
+            box3.append(board[i+j])
+        no_zero_box_3=list(filter(lambda a: a != 0, box3))
+        if len(no_zero_box_3)!=len(set(no_zero_box_3)):
+            return False
+    
+    box1=[]
+    box2=[]
+    box3=[]
+    for i in ROW[6:9]:   
+        for j in COL[0:3]:
+            box1.append(board[i+j])
+        no_zero_box_1=list(filter(lambda a: a != 0, box1))
+        if len(no_zero_box_1)!=len(set(no_zero_box_1)):
+            return False
+        for j in COL[3:6]:   
+            box2.append(board[i+j])
+        no_zero_box_2=list(filter(lambda a: a != 0, box2))
+        if len(no_zero_box_2)!=len(set(no_zero_box_2)):
+            return False
+        for j in COL[6:9]:
+            box3.append(board[i+j])
+        no_zero_box_3=list(filter(lambda a: a != 0, box3))
+
+        if len(no_zero_box_3)!=len(set(no_zero_box_3)):
+            return False
+    return True
